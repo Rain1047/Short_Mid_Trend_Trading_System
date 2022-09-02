ea7a3e9bdf4a2937be7f0a7ca8a8ce413b379ca1
Rain1047 <3487851868@qq.com>
2022-09-02 17:56:09 +0800

    better

--- scripts/gather_data/script_demo.py
+++ scripts/gather_data/script_demo.py
@@ -1,22 +1,23 @@
 # import scripts
-from script_get_eps import get_eps
-from script_get_eps import get_roe
-from script_get_eps import get_pe
+from script_get_fundmentals_us import get_eps
+from script_get_fundmentals_us import get_roe
+from script_get_fundmentals_us import get_pe
+from script_get_fundmentals_us import get_npm
 # multiprocessing
 import multiprocessing
 
 
 if __name__ == '__main__':
 
-    p_roe = multiprocessing.Process(target=get_roe, args=(2553,)) 
-    p_pe = multiprocessing.Process(target=get_pe, args=(325,))
+    # p_roe = multiprocessing.Process(target=get_roe, args=(2553,)) 
+    # p_pe = multiprocessing.Process(target=get_pe, args=(325,))
 
-    p_roe.start()
-    p_pe.start()
+    # p_roe.start()
+    # p_pe.start()
 
-    p_roe.join()
-    p_pe.join()
-
+    # p_roe.join()
+    # p_pe.join()
+    get_npm()
     print('Done')
 
 # get_roe(2553) # last run: 2022/8/25 22:32
