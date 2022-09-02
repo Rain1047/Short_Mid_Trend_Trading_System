fe8a19ddd828172340493e8c934943879b072feb
Rain1047 <3487851868@qq.com>
2022-09-03 00:39:12 +0800

    fix

--- scripts/gather_data/script_demo.py
+++ scripts/gather_data/script_demo.py
@@ -1,22 +1,23 @@
-# import scripts
-from script_get_eps import get_eps
-from script_get_eps import get_roe
-from script_get_eps import get_pe
+# # import scripts
+# from script_get_eps import get_eps
+# from script_get_eps import get_roe
+# from script_get_eps import get_pe
+from script_get_fundamentals_us import get_rev 
 # multiprocessing
 import multiprocessing
 
 
 if __name__ == '__main__':
 
-    p_roe = multiprocessing.Process(target=get_roe, args=(2553,)) 
-    p_pe = multiprocessing.Process(target=get_pe, args=(325,))
+    # p_roe = multiprocessing.Process(target=get_roe, args=(2553,)) 
+    # p_pe = multiprocessing.Process(target=get_pe, args=(325,))
 
-    p_roe.start()
-    p_pe.start()
+    # p_roe.start()
+    # p_pe.start()
 
-    p_roe.join()
-    p_pe.join()
-
+    # p_roe.join()
+    # p_pe.join()
+    get_rev(300)
     print('Done')
 
 # get_roe(2553) # last run: 2022/8/25 22:32
